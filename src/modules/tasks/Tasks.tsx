73428cf17a42fee51661a58932beacf3dfee1005
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2025-06-02 21:28:50 +0300

    add accordion

--- src/modules/tasks/Tasks.tsx
+++ src/modules/tasks/Tasks.tsx
@@ -1,4 +1,5 @@
 import useGetTasks from "@/common/hooks/useGetTasks";
+import Accordion from "@/common/ui/accordion/Accordion";
 import PreviewTask from "@/modules/tasks/components/previewTask/PreviewTask";
 import "@/modules/tasks/style.scss";
 
